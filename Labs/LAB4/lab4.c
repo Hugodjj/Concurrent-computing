c5cbe1e48e96e5691dfb2e3b6f1fca2f2b337245
Hugo Nascimento <hugons@dcc.ufrj.br>
2022-05-13 17:40:34 -0300

    laboratório 4 finalizado

---
+++ Labs/LAB4/lab4.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <pthread.h>
+#include <time.h>
+#include <math.h>
+#include "timer.h"
+
+long long int dim;
+int *vetorEntrada, nthreads, varGlobal;
+float *vetorSequencial, *vetorConcorrente;
+pthread_mutex_t mutex;
+pthread_t *tid;
+
+int ehPrimo(long long int n) {
+    if (n<=1)
+    return 0;
+    
+    if (n==2)
+    return 1;
+    
+    if (n%2==0)
+    return 0;
+    
+    for (int i=3; i<sqrt(n)+1; i+=2)
+        if(n%i==0) return 0;
+    return 1;
+}
+
+void *tarefa(void* arg){
+    float* vetorThreads = (float *) arg;
+    int i = 0;
+    while(i < dim){
+
+        pthread_mutex_lock(&mutex);
+        i = varGlobal++;
+        pthread_mutex_unlock(&mutex);
+
+        if (ehPrimo(vetorEntrada[i]))
+            vetorThreads[i] = sqrt(vetorEntrada[i]);
+        else
+            vetorThreads[i] = vetorEntrada[i];
+
+    }
+    pthread_exit(NULL);
+}
+
+void gerar_vet(int dim){
+
+    srand(time(NULL));
+    for (int i = 0; i<dim; i++){
+        vetorEntrada[i] = rand() % 10000000000;
+    }
+}
+
+void alocar_memo(int dim){
+
+    vetorEntrada = (int *) malloc(sizeof(int)*dim);
+    if (vetorEntrada == NULL){
+        printf("ERRO-- malloc 1\n");
+        exit(-1);
+    }
+
+    vetorSequencial = (float *) malloc(sizeof(float)*dim);
+    if (vetorSequencial == NULL){
+        printf("ERRO-- malloc 2\n");
+        exit(-1);
+    }
+
+    vetorConcorrente = (float *) malloc(sizeof(float)*dim);
+    if (vetorSequencial == NULL){
+        printf("ERRO-- malloc 3\n");
+        exit(-1);
+    }
+
+    tid = (pthread_t *)malloc(sizeof(pthread_t) * nthreads);
+    if (tid == NULL){
+        fprintf(stderr, "ERRO--malloc 4\n");
+        exit(-1);
+    }
+}
+
+void free_memo(){
+    free(vetorConcorrente);
+    free(tid);
+    free(vetorSequencial);
+    free(vetorEntrada);
+}
+
+int main(int argc, char *argv[]){
+
+    double inicio, fim;
+
+    if (argc < 3){
+        fprintf(stderr, "Digite: %s <dimensao do vetor> <numero threads>\n", argv[0]);
+        return 1;
+    }
+    dim = atoll(argv[1]);
+    nthreads = atoi(argv[2]);
+
+    alocar_memo(dim);
+    gerar_vet(dim);
+
+    // printf("\n\nMATRIZZZ\n\n");
+    // for (long long int i = 0; i< dim; i++){
+    //     printf("%d ", vetorEntrada[i]);
+    // }
+
+    GET_TIME(inicio);
+    for (long long int i = 0; i< dim; i++){
+
+        if (ehPrimo(vetorEntrada[i]))
+            vetorSequencial[i] = sqrt(vetorEntrada[i]);
+        else
+            vetorSequencial[i] = vetorEntrada[i];
+    }
+    GET_TIME(fim);
+    printf("\nTEMPO TEMPO TEMPO SEQUENCIAL: %lf\n", fim - inicio);
+
+    // for (long long int i = 0; i< dim; i++){
+    //     printf("%.1lf ", vetorSequencial[i]);
+    // }
+
+    GET_TIME(inicio);
+    pthread_mutex_init(&mutex,NULL);
+
+    for (long int i = 0; i < nthreads; i++){
+        if (pthread_create(tid + i, NULL, tarefa, (void *)vetorConcorrente)){
+            fprintf(stderr, "ERRO--pthread_create\n");
+            return 3;
+        }
+    }
+
+    for (long int i = 0; i < nthreads; i++){
+        if (pthread_join(*(tid + i), NULL)){
+            fprintf(stderr, "ERRO--pthread_create\n");
+            return 3;
+        }
+    }
+    pthread_mutex_destroy(&mutex);
+    GET_TIME(fim);
+    printf("\n\nTEMPO CONCORRENTEEE: %lf\n\n", fim - inicio);
+
+    free_memo();
+
+    return 0;
+}
