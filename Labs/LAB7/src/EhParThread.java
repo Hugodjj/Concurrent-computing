6654b498d0a0fdea8985a189592aded24c36cabe
Hugo Nascimento <hugons@dcc.ufrj.br>
2022-06-16 19:29:57 -0300

    adicionando nome e comentários

--- Labs/LAB7/src/EhParThread.java
+++ Labs/LAB7/src/EhParThread.java
@@ -1,41 +1,55 @@
+/* Disciplina: Computacao Concorrente */
+/* Prof.: Silvana Rossetto */
+/* Módulo 2 - Laboratório: 7 */
+/* Codigo: "Esse programa verifica quantos numeros pares existem em um array de numeros inteiros." usando threads em C*/
+/* Nome: Hugo Nascimento da Silva*/
+/* DRE: 120152161*/
+
 import java.util.ArrayList;
+
+//Classe que para recursos compartilhados entre as threads
 
 class S1{
 
+    // recursos compartilhados
     private ArrayList<Integer> numeros;
     private int num;
 
-
+    // construtor da classe
     public S1(ArrayList<Integer> numeros) {
         this.numeros = numeros;
         this.num = 0;
     }
 
+    // métodos de sincronização entre as threads
     public synchronized void inc(){
         this.num++;
     }
-
     public synchronized int get(){
         return this.num;
     }
-
     public synchronized int get(int x){
         return this.numeros.get(x);
     }
 
 }
 
+//classe que estende Thread e implementa a tarefa de cada thread do programa
 class T1 extends Thread{
+    // id da thread
     private int id;
 
+    // objeto compartilhado com as outras threads
     S1 objetoS;
 
 
+    // construtor
     public T1(int id, S1 objetoS) {
         this.id = id;
         this.objetoS = objetoS;
     }
 
+    // método main da thread
     public void run(){
         for (int i = id; i < 100000; i+= EhParThread.NT){
             if((this.objetoS.get(i)%2) == 0){
@@ -45,26 +59,35 @@ class T1 extends Thread{
     }
 }
 
+// classe principal
 public class EhParThread {
 
     public static final int NT = 2;
 
     public static void main(String[] args) {
+        // vetor de numeros
         ArrayList<Integer> arrayNumeros = new ArrayList<>();
+        // vetor de threads
         Thread[] threads = new Thread[NT];
 
+        // instanciando um objeto para utilizar recursos compartilhados
+        S1 s1 = new S1(arrayNumeros);
+
+        // preenchendo o array
         for (int i = 0; i< 100000; i++){
             arrayNumeros.add(i);
         }
-        S1 s1 = new S1(arrayNumeros);
-
+        // criação das threads
         for (int i = 0; i< threads.length; i++){
             threads[i] = new T1(i,s1);
         }
 
+        // inicio das threads
         for (int i = 0; i<threads.length; i++){
             threads[i].start();
         }
+
+        // espera todas as threads terminarem.
         for (int i = 0; i< threads.length; i++){
             try { threads[i].join(); } catch (InterruptedException e) { return; }
         }
